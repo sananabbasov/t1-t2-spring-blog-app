e07ca40d473313e2dcdfe9b152984d5c2e5d6723
sananabbasov <sanan_abbasov@list.ru>
2024-05-30 01:27:03 +0400

    errorrrrrrrrrrrrrrrrrrrrrr

--- src/main/java/itbrains/az/blog/config/Configure.java
+++ src/main/java/itbrains/az/blog/config/Configure.java
@@ -5,6 +5,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 public class Configure {
@@ -14,5 +16,18 @@ public class Configure {
     public ModelMapper modelMapper()
     {
         return new ModelMapper();
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+        ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+        secondaryTemplateResolver.setPrefix("templates-2/");
+        secondaryTemplateResolver.setSuffix(".html");
+        secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+        secondaryTemplateResolver.setOrder(1);
+        secondaryTemplateResolver.setCheckExistence(true);
+
+        return secondaryTemplateResolver;
     }
 }
