f779a7a0893e4933d2dbadc604ed2c1ecc7a254d
sananabbasov <sanan_abbasov@list.ru>
2024-05-30 01:45:53 +0400

    j

--- src/main/java/itbrains/az/blog/controller/HomeController.java
+++ src/main/java/itbrains/az/blog/controller/HomeController.java
@@ -25,14 +25,14 @@ public class HomeController {
     private ArticleService articleService;
     @Autowired
     private CategoryService categoryService; // 123
-    @GetMapping("/")
-    public String index(Model model)
-    {
-        List<ArticleHomeDto> homeArticles =  articleService.getHomeArticles();
-        model.addAttribute("articles",homeArticles);
-        return "error";
-    }
-
+//    @GetMapping("/")
+//    public String index(Model model)
+//    {
+//        List<ArticleHomeDto> homeArticles =  articleService.getHomeArticles();
+//        model.addAttribute("articles",homeArticles);
+//        return "error";
+//    }
+//
 
     @GetMapping("/home")
     public String home(Model model)
