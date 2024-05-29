e8816ad468af5838751f69783460a892cdd9541c
sananabbasov <sanan_abbasov@list.ru>
2024-05-30 00:57:51 +0400

    errorrrrrr

---
+++ src/main/java/itbrains/az/blog/controller/TestController.java
@@ -0,0 +1,13 @@
+package itbrains.az.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test"; // Ensure test.html exists in the templates directory
+    }
+}
